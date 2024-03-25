42c12b0981333c411bb98d9c9afc7a1153730808
AhmedMohamedAbdelmohsen <33791214+AhmedMohamedAbdelmohsen@users.noreply.github.com>
2024-03-25 14:52:44 +0200

    update

--- lib/calendar_tile.dart
+++ lib/calendar_tile.dart
@@ -93,7 +93,7 @@ class NeatCleanCalendarTile extends StatelessWidget {
       return GestureDetector(
         onTap: onDateSelected, // react on tapping
         child: Padding(
-          padding: const EdgeInsets.all(5.0),
+          padding: const EdgeInsets.all(2.0),
           child: Container(
             // If this tile is the selected date, draw a colored circle on it. The circle is filled with
             // the color passed with the selectedColor parameter or red color.
